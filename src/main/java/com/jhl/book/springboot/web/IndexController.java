405b808cd82fa2707ebe6851d2973d8a361779b1
Jung-hyelim <jhl3646@gmail.com>
2021-03-10 13:56:23 +0900

    로그인 세션 정보를 어노테이션 기반으로 개선

--- src/main/java/com/jhl/book/springboot/web/IndexController.java
+++ src/main/java/com/jhl/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jhl.book.springboot.web;
 
+import com.jhl.book.springboot.config.auth.LoginUser;
 import com.jhl.book.springboot.config.auth.dto.SessionUser;
 import com.jhl.book.springboot.service.PostsService;
 import com.jhl.book.springboot.web.dto.PostsResponseDto;
@@ -16,13 +17,13 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");    // @LoginUser 어노테이션으로 개선
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
